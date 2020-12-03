200dd16b9824375aaa51aaa3690a38eef63ac498
TDTran333 <56975352+TDTran333@users.noreply.github.com>
2020-12-03 13:59:41 -0500

    Updated chart test file

--- code/graph_test.R
+++ code/graph_test.R
@@ -40,35 +40,7 @@ pi %>%
   geom_bar( stat = "identity")
 
 
-pi <- df[8:10] %>% 
-  bind_rows() %>% 
-  select(pipos, pizero, pineg) %>%
-  arrange(desc(pipos), pineg, pizero)
-
-cex = cex.axis = 0.8
-par(mfrow = c(1, 2))
-plot(12 * 100 * sort(df$alpha, decreasing = TRUE), 1:length(df$alpha), type = 'b', las = 1, pch = 20, cex = cex, cex.axis = cex.axis, 
-     xlab = "", ylab = "", main = "alpha", axes = FALSE)
-box(); grid()
-labs = seq(-30, 30, by = 10)
-axis(side = 1, at = labs, labels = paste0(labs, "%"), cex.axis = cex.axis)
-axis(side = 2, at = 1:length(df$alpha), labels = 1:length(df$alpha), las = 1, cex.axis = cex.axis)
-
-mainstr = expression(hat(pi)^'+'*' / '*hat(pi)^0*' / '*hat(pi)^'-')
-barplot(t(100 * pi), horiz = TRUE, names.arg = NULL, col = c(gray(0), gray(0.8), gray(0.5)),
-        space = 0, xlab = "", xlim = c(0, 100), cex.names = cex.axis, border = NA,
-        cex.axis = cex.axis, main = mainstr, las = 1, axes = FALSE)
-labs = seq(0, 100, by = 25)
-axis(side = 1, at = labs, labels = paste0(labs, "%"), cex.axis = cex.axis)
-box()
-x = seq(from = 104, to = -4, length.out = 200)
-y = seq(from = 0, to = 100, length.out = 200)
-par(new = TRUE); plot(x, y, type = 'l', lty = "dashed", lwd = 1, xlim = c(0, 100), ylim = c(0, 100), axes = FALSE, ylab = "", xlab = "")
-par(new = TRUE); plot(x-27.5, y, type = 'l', lty = "dashed", lwd = 1, xlim = c(0, 100), ylim = c(0, 100), axes = FALSE, ylab = "", xlab = "")
-par(new = TRUE); plot(x+27.5, y, type = 'l', lty = "dashed", lwd = 1, xlim = c(0, 100), ylim = c(0, 100), axes = FALSE, ylab = "", xlab = "")
-
-
-make_graphs <- functions(df) {
+f_make_graphs <- function(df) {
   pi <- df[8:10] %>% 
     bind_rows() %>% 
     select(pipos, pizero, pineg) %>%
@@ -97,3 +69,4 @@ make_graphs <- functions(df) {
   par(new = TRUE); plot(x+27.5, y, type = 'l', lty = "dashed", lwd = 1, xlim = c(0, 100), ylim = c(0, 100), axes = FALSE, ylab = "", xlab = "")
 }
 
+f_make_graphs(df)
